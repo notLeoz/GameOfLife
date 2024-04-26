6b698eff519976ed6a1c647f957d4a2963d3c8ac
Leonardo Lucato <leonardolucato@MBPdiLeonardo3.wind3.hub>
2024-04-27 01:14:07 +0200

    Created basic logic for game of life

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-const MAT_SIZE: usize = 10;
+use std::{thread, time::Duration};
+
+const MAT_SIZE: usize = 40;
+const GENERATIONS: u8 = 140;
+const GEN_SPEED: u64 = 200;
+
 
 fn get_sum_neighbors(world: &[[u8; MAT_SIZE]; MAT_SIZE], pos_x: u32,pos_y: u32) -> u32 {
     let mut sum: u32 = 0;
@@ -22,8 +27,62 @@ fn display_world(world: &[[u8; MAT_SIZE]; MAT_SIZE]) {
         println!();
     });
 }
+
+fn get_new_cell_value(is_curr_cell_alive: bool,sum_neighbors: u32) -> u8 {
+    match is_curr_cell_alive {
+       true => {
+        match sum_neighbors {
+           0..=1 => 0,
+           2..=3 => 1,
+           _ => 0
+        }
+       } 
+       false => {
+        match sum_neighbors {
+            3 => 1,
+            _ => 0
+        }
+       }
+    }
+}
+
+fn create_new_world(curr_world:&[[u8; MAT_SIZE];MAT_SIZE]) -> [[u8; MAT_SIZE];MAT_SIZE] {
+    let mut new_world = [[0u8; MAT_SIZE]; MAT_SIZE];
+    for i in 0..MAT_SIZE {
+        for j in 0..MAT_SIZE {
+            new_world[i][j] = get_new_cell_value(if curr_world[i][j] == 1 {true} else {false}, get_sum_neighbors(curr_world, i as u32, j as u32));
+        }
+    }
+    new_world
+}
+
+fn copy_new_world(curr_world:&mut[[u8; MAT_SIZE]], new_world: [[u8;MAT_SIZE];MAT_SIZE]) {
+    for i in 0..MAT_SIZE {
+        for j in 0..MAT_SIZE {
+            curr_world[i][j] = new_world[i][j];
+        }
+    }
+}
+
+fn apply_die_hard_configuration(world:&mut[[u8; MAT_SIZE]]) {
+    world[14][18] = 1;
+    world[16][17] = 1;
+    world[16][18] = 1;
+    world[16][19] = 1;
+    world[15][12] = 1;
+    world[15][13] = 1;
+    world[16][13] = 1;
+}
+
 fn main() {
-    const MAT_SIZE : usize = 10;
     let mut world = [[0u8; MAT_SIZE]; MAT_SIZE];
-    display_world(&world);
+    apply_die_hard_configuration(&mut world);
+    for _ in 0..GENERATIONS {
+        print!("{}[2J", 27 as char);
+        display_world(&world);
+        thread::sleep(Duration::from_millis(GEN_SPEED));
+        let new_world = create_new_world(&world);
+        copy_new_world(&mut world, new_world);
+        
+    }
 }
